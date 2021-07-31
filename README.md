1a134e9d549f313efe17e26ad20da18fd4a9a032
YashJain <yjain3.436@gmail.com>
2021-07-31 18:06:25 +0530

    worked on login-logout + added Homepage

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # QuestionOverflow
+
 I'll do It
+
+1. npm init
+2. install all dependencies
+3. nodemon : npm install nodemon -g
+4. changes to be made : in package.json file
+
+```
+"scripts": {
+    "start": "node app.js",
+    "devStart": "nodemon app.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  }
+```
+
+5. to run our server : npm run devStart
---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+//db conections
+const dbURI =
+  "mongodb+srv://yash:jain123@cluster0.wasy4.mongodb.net/DSAOVERFLOW";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(3000, function () {
+      console.log("Server is running on Port 3000");
+    })
+  )
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.use(authRoutes);
