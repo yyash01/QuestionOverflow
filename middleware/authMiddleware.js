25d583b55a9a0c2893097153eefb067ed89ae265
YashJain <yjain3.436@gmail.com>
2021-08-15 15:03:54 +0530

    nothing much changed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
-
   // check json web token exists & is verified
   if (token) {
     jwt.verify(token, "net ninja secret", (err, decodedToken) => {
